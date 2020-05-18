ea5cabca6a05ef6c525cd97389923daf3dd7da1c
wakamezake <hotwater1367@gmail.com>
2020-05-18 15:43:41 +0900

    add comment

--- nlplot/helper.py
+++ nlplot/helper.py
@@ -1,4 +1,10 @@
 def is_notebook() -> bool:
+    """ Check if the execution environment is an interactive shell or a jupyter_notebook.
+    Returns:
+        True or False
+    reference:
+        https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+    """
     try:
         # get_ipython function is an interactive shell or,
         # in the case of jupyter_notebooks, an already-loaded
