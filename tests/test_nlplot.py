34d01f530e7a9cefeeb41012971c10640ed2a9b0
wakamezake <hotwater1367@gmail.com>
2020-05-23 23:36:52 +0900

    add test

--- tests/test_nlplot.py
+++ tests/test_nlplot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from nlplot import NLPlot
+from nlplot import NLPlot, get_colorpalette, freq_df
 
 
 @pytest.fixture
@@ -19,6 +19,41 @@ def prepare_data():
 def prepare_instance(prepare_data):
     npt = NLPlot(prepare_data, taget_col="text")
     return npt
+
+
+@pytest.fixture
+def apply_space(prepare_data, target_col="text"):
+    df = prepare_data
+    return df[target_col].apply(lambda x: ' '.join(x))
+
+
+@pytest.mark.parametrize("color_palette,"
+                         " n_legends",
+                         [pytest.param("hls", 0, marks=pytest.mark.xfail),
+                          ("hls", 1),
+                          ("hls", 10),
+                          ("hls", 100)])
+def test_get_colorpalette(color_palette, n_legends):
+    rgbs = get_colorpalette(color_palette, n_legends)
+    assert isinstance(rgbs, list)
+    assert len(rgbs) != 0
+
+
+@pytest.mark.parametrize("n_gram", [pytest.param(0, marks=pytest.mark.xfail),
+                                    1, 2, 3])
+@pytest.mark.parametrize("top_n", [pytest.param(0, marks=pytest.mark.xfail),
+                                   1, 10, 50])
+def test_freq_df(apply_space, n_gram, top_n):
+    stop_words = []
+    verbose = False
+    df = apply_space
+    word_frequency = freq_df(df, n_gram=n_gram, n=top_n,
+                             stopwords=stop_words, verbose=verbose)
+    expect_columns = ["word", "word_count"]
+    assert isinstance(word_frequency, pd.DataFrame)
+    assert word_frequency.ndim == 2
+    assert len(word_frequency) <= top_n
+    assert list(word_frequency.columns) == expect_columns
 
 
 def test_nlplot_bar_ngram(prepare_instance):
